2cf81b3bf74e440f98c891400cf4086e72de031d
Syahrul Ilham Muttaqien <syahrulilham62@gmail.com>
2024-03-13 22:35:53 +0700

    Test 1 - LOOP ALGORITHM

--- src/test/java/com/example/test/TestApplicationTests.java
+++ src/test/java/com/example/test/TestApplicationTests.java
@@ -8,6 +8,13 @@ class TestApplicationTests {
 
 	@Test
 	void createdLoopAlgo() {
+		int loop = 5;
+		for (int i = 1; i <= loop; i++) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print(i+"");
+			}
+			System.out.println();
+		}
 		
 	}
 
